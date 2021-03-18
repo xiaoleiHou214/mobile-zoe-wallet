74bafc757bf24e568d77cfc4caab60951a6597bc
BryantDev <47358033+BryantDev@users.noreply.github.com>
2021-03-17 23:04:46 -0500

    .

--- app/src/main/java/com/alphawallet/app/C.java
+++ app/src/main/java/com/alphawallet/app/C.java
@@ -40,7 +40,7 @@ public abstract class C {
     public static final String POA_TICKER = "poa";
 
     public static final String USD_SYMBOL = "$";
-    public static final String ETH_SYMBOL = "ETH";
+    public static final String ETH_SYMBOL = "ZOE";
     public static final String xDAI_SYMBOL = "xDai";
     public static final String POA_SYMBOL = "POA";
     public static final String ETC_SYMBOL = "ETC";
